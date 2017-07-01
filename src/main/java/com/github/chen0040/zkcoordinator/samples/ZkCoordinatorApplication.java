033298cb7123a1c29cbcd82f2f08820aaff68f79
xschen <xs0040@gmail.com>
2017-07-01 14:32:07 +0800

    spring based client web view

---
+++ src/main/java/com/github/chen0040/zkcoordinator/samples/ZkCoordinatorApplication.java
@@ -0,0 +1,19 @@
+package com.github.chen0040.zkcoordinator.samples;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+/**
+ * Created by xschen on 1/7/2017.
+ */
+@SpringBootApplication
+@EnableScheduling
+public class ZkCoordinatorApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(ZkCoordinatorApplication.class, args);
+   }
+}
